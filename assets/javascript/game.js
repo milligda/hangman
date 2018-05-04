697c2df25d7599f0c1d22cb85bd2149d4b6440e9
DMilligan <dmilligan@springlocker.com>
2018-05-04 12:24:09 -0400

    fixing styling and adding comments to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // the list of game words
     var words = [
         "yosemite",
         "glacier",
@@ -24,53 +25,66 @@ $(document).ready(function() {
     var losses = 0;
     var guessesStart = 5
 
-
+    // starts a new game
     function newGame() {
 
+        // resets the variables
         guessesRemaining = guessesStart;
         lettersCorrectlyGuessed = [];
         lettersGuessed = [];
         answerArray = [];
         
-        // randomly select a word for the new game
+        // randomly selects a word for the new game
         gameWord = words[Math.floor(Math.random() * words.length)];
 
         // splits the selected word into an array comprised of each character as a separate item
         correctLetters = gameWord.split("");
 
+        // populates the answerArray with blank spaces
         for (var i = 0; i < correctLetters.length; i++) {
             answerArray[i] = "_";
         }
 
+        // changes the background image to the image that corresponds with the game word
         $("body").css("background-image", `url("assets/images/${gameWord}.jpg")`);
 
     }
 
+    // looks at the key the player enters, determines if it is valid, and compares it to the correctLetters array
     function singlePlay(userGuess) {
 
         var playableKey = false;
 
+        // array of acceptable keys a user can enter
         var playOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
                            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 
+        // Compares the userGuess to the playable keys to determine if the key is playable.  
         for (var i = 0; i < playOptions.length; i++) {
             if (userGuess === playOptions[i]) {
                 playableKey = true;
             }
         }
 
+        // Compares the userGuess to letters they have already guessed so they cannot guess the same letter twice
         for (var j = 0; j < lettersGuessed.length; j++) {
             if (userGuess === lettersGuessed[j]) {
                 playableKey = false;
             }
         }
 
+        // If the userGuess is valid, run it through the game play
         if (playableKey) {
             correctAnswer = false;
+
+            // adds the userGuess to the array of the letters they have already guessed
             lettersGuessed.push(userGuess);
 
+            // compares the userGuess to the correct letters
             for (var i = 0; i < correctLetters.length; i++) {
             
+                // if the userGuess is one of the correct letters, include it in the letters correctly guessed array
+                // and display it in the answerArray.  
                 if (userGuess === correctLetters[i]) {
                     correctAnswer = true;
                     answerArray[i] = userGuess;
@@ -78,32 +92,33 @@ $(document).ready(function() {
                 }
             }
 
+            // if the userGuess was incorrect, decrease the guesses remaining by 1
             if (correctAnswer) {
                 guessesRemaining = guessesRemaining;
             } else {
                 guessesRemaining = guessesRemaining - 1;
             }
-
-            newKey = false;
         }
-
     }
 
+    // Determines if the game has been won, lost or should continue
     function determineNextPlay() {
 
+        // compares the correct guesses to the correct letters.  If they match, the game is won and a new game is started
         if(lettersCorrectlyGuessed.length === correctLetters.length) {
             console.log("You won!!!");
             wins++;
             newGame();
 
+        // if they have not won, look at the number of guesses remaining to see if they lost the game or if the play should continue
         } else if (guessesRemaining === 0) {
             console.log("You lost.");
             losses++;
             newGame();
-
         } 
     }
 
+    // Displays the game stats on the page
     function displayStats() {
 
         $("#game-stats").html(
@@ -113,20 +128,26 @@ $(document).ready(function() {
             );
     }
 
+    // displays the game play on the page
     function displayGame() {
         
         var guessesDisplay = "";
 
+        // Adds the letters guessed display
         for (var i = 0; i < lettersGuessed.length; i++) {
             guessesDisplay = lettersGuessed[i] + " " + guessesDisplay;
         }
 
+        // creates a <p> element with the guesses
         var guessesP = $("<p>" + guessesDisplay + "</p>");
 
+        // replaces the guesses-div with the <p> element of the guesses
         $("#guesses-div").html(guessesP);
 
+        // creates a <p> element for the progress
         var progressP = $("<p>" + answerArray.join(" ") + "</p>");
         
+        // displays the progress on the page
         $("#progress").html(progressP);
 
     }
